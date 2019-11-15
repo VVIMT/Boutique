282233c618c7caeb0cffcb1ecb29282fc16c556e
root <root@vps692299.ovh.net>
2019-11-15 18:10:53 +0100

    Boutique

---
+++ inc/init.inc.php
@@ -0,0 +1,24 @@
+<?php
+//--------- BDD
+//$mysqli = new mysqli("51.83.74.152", "root", ")aWp05QZQ(", "site");
+//if ($mysqli->connect_error) die('Un probleme est survenu lors de la tentative de connexion a la BDD : ' . $mysqli->connect_error);
+
+$dbhost = "51.83.74.152";
+$dbuser = "root";
+$dbpass = ")aWp05QZQ(";
+$db = "boutique";
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+// $mysqli->set_charset("utf8");
+ 
+//--------- SESSION
+session_start();
+
+//--------- CHEMIN
+define("RACINE_SITE","/base_site/");
+ 
+//--------- VARIABLES
+$contenu = '';
+ 
+//--------- AUTRES INCLUSIONS
+require_once("fonction.inc.php");
